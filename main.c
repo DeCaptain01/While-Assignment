ed42ca2cb0e22e4b85c4238ef35f3a4535661d24
DeCaptain01 <125448534+DeCaptain01@users.noreply.github.com>
2024-02-28 23:48:45 +0300

    My Commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num=53;
+    while(num>=21){
+        if(num%2!=0){
+            printf("%d\n", num);
+        }
+        num--;
+    }
+
+    return 0;
+}
